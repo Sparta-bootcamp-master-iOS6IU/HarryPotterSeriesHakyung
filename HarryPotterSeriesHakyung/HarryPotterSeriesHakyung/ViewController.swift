d18ef2a54e9fa442269d7c6ba3335d7cdb1ded1d
Hakyung Sohn <hortenssiaa@gmail.com>
2025-03-28 14:42:16 +0900

    🔧 Config: 프로젝트 환경 및 데이터 설정
    코드 베이스 UI개발 환경설정, Assets 이미지 추가, data.json 파일 추가, Package Dependencies에 Static Snapkit Framework 추가하였음

---
+++ HarryPotterSeriesHakyung/HarryPotterSeriesHakyung/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HarryPotterSeriesHakyung
+//
+//  Created by kingj on 3/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ HarryPotterSeriesHakyung/HarryPotterSeriesHakyungTests/HarryPotterSeriesHakyungTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterSeriesHakyungTests.swift
+//  HarryPotterSeriesHakyungTests
+//
+//  Created by kingj on 3/28/25.
+//
+
+import Testing
+@testable import HarryPotterSeriesHakyung
+
+struct HarryPotterSeriesHakyungTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
