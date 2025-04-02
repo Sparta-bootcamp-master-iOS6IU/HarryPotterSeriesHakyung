766546bc9baa57e709144a9a2a4c4d253d7e9887
Hakyung Sohn <hortenssiaa@gmail.com>
2025-04-03 01:01:16 +0900

    ✨ Feat: #17 Level 5. Summary 접기/더보기 기능 구현 - 글자수가 450자 이상인 경우 … 말줄임표 표시 및 더보기 버튼 표시   - 예외사항     - 2권(시리즈 두번째)의 요약 내용은 글자수가 450자 미만이므로 더보기 버튼이 표시되지 않아야 한다. - 더보기 버튼을 누르면 요약 텍스트 전체가 표시되고 더보기 버튼은 접기 버튼으로 변경   - 더보기 버튼을 눌러 Summary를 펼친 상태로 앱을 종료했다면, 앱을 다시 실행했을 때 펼쳐진 상태로 표시되어 있어야 한다.   - 반대로 접기버튼을 눌러 접은 상태로 종료했다면 앱 종료 후 다시 실행했을 때 접은 상태로 표시되어 있어야 한다. - + 파일 이름 변경 HarryPotterViewModel.swift -> HomeViewModel.swift

---
+++ HarryPotterSeriesHakyung/HarryPotterSeriesHakyung/View/MoreLessButton.swift
@@ -0,0 +1,8 @@
+//
+//  MoreLessButton.swift
+//  HarryPotterSeriesHakyung
+//
+//  Created by kingj on 4/2/25.
+//
+
+import Foundation
--- HarryPotterSeriesHakyung/HarryPotterSeriesHakyung/ViewModel/HomeViewModel.swift
+++ HarryPotterSeriesHakyung/HarryPotterSeriesHakyung/ViewModel/HomeViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-final class HarryPotterViewModel {
+final class HomeViewModel {
     
     // MARK: - Properties
     
